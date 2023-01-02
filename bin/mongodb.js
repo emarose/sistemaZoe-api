630acdc4a4c176efb06929b0eaec5a7951a79d02
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2023-01-02 10:28:21 -0300

    .env

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,8 +1,7 @@
 var mongoose = require("mongoose");
 
-const url =
-  "mongodb+srv://emarose:12Metallica12@cluster0.wj35z.mongodb.net/sistemaZoe?retryWrites=true&w=majority";
-
+const url = process.env.MONGO_URL;
+mongoose.set("strictQuery", false);
 mongoose.connect(
   url,
   {
